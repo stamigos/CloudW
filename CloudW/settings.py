0f832788756454e2be23a91d1e3c234680209b08
stamigos <oldtigersvoice@gmail.com>
2015-04-07 20:43:25 +0300

    prduction 0.3

--- CloudW/settings.py
+++ CloudW/settings.py
@@ -104,9 +104,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = ''
+STATIC_ROOT = 'htdocs'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/htdocs/static/'
 
 #STATICFILES_DIRS = (
 #    os.path.join(BASE_DIR, 'static'),
