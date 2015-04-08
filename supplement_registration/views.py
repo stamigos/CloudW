520b573f00a850d08a76d8c88cb00427dd194a1e
stamigos <oldtigersvoice@gmail.com>
2015-04-08 20:53:50 +0300

    edit profile

--- supplement_registration/views.py
+++ supplement_registration/views.py
@@ -12,9 +12,7 @@ from supplement_registration.models import MyRegistrationSupplement
 @login_required(login_url='/registration/register')
 def profile_settings(request, template_name="person_card.html"):
     profile = MyRegistrationSupplement.objects.get(pk=request.user.pk)
-    if request.GET:
-        profile = MyRegistrationSupplement.objects.get(pk=request.user.pk)
-    profile = "jfjvnkjfd"
+
     if request.POST:
         employee = MyRegistrationSupplement.objects.get(pk=request.user.pk)
         employee.first_name = request.POST.get('first_name')
@@ -33,12 +31,12 @@ def profile_settings(request, template_name="person_card.html"):
         return HttpResponseRedirect('/person_card/')
 
     return render_to_response(template_name, {
-        'first_name': profile,
-        'last_name': profile,
-        'father_name': request.GET.get('father_name'),
-        'city': request.GET.get('city'),
-        'email': request.GET.get('email'),
-        'phone_number': request.GET.get('phone_number')
+        'first_name': profile.first_name,
+        'last_name': profile.last_name,
+        'father_name': profile.father_name,
+        'city': profile.city,
+        'email': profile.email,
+        'phone_number': profile.phone_number
     }, context_instance=RequestContext(request))
 
 
