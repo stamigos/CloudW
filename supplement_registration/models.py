38970e3ae45ae05512491a4facf9219918c541e8
stamigos <oldtigersvoice@gmail.com>
2015-04-09 13:41:31 +0300

    help fields

--- supplement_registration/models.py
+++ supplement_registration/models.py
@@ -7,12 +7,12 @@ from django.contrib.auth.models import User
 
 class MyRegistrationSupplement(RegistrationSupplementBase):
 
-    first_name = models.CharField(u'Имя', max_length=100) #help_text=u'Введите свое имя'
-    last_name = models.CharField(u'Фамилия', max_length=30) #help_text=u'Введите свою фамилию')
-    father_name = models.CharField(u'Отчество', max_length=30) #help_text=u'Введите свое отчество')
-    city = models.CharField(u'Город', max_length=30) #help_text=u'Введите город')
-    email = models.EmailField(u'Email', max_length=100)
-    phone_number = PhoneNumberField(u'Телефон', max_length=30) #help_text=u'Введите свою фамилию')
+    first_name = models.CharField(u'Имя', max_length=100, help_text=u'Введите свое имя')
+    last_name = models.CharField(u'Фамилия', max_length=30, help_text=u'Введите свою фамилию')
+    father_name = models.CharField(u'Отчество', max_length=30, help_text=u'Введите свое отчество')
+    city = models.CharField(u'Город', max_length=30, help_text=u'Введите город')
+    email = models.EmailField(u'Email', max_length=100, help_text=u'Введите email')
+    phone_number = PhoneNumberField(u'Телефон', max_length=30, help_text=u'Введите свою фамилию')
 
     def __unicode__(self):
         # a summary of this supplement
