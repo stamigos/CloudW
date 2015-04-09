51b8c52ef40d92932a889f0591a23a1d53f9424c
stamigos <oldtigersvoice@gmail.com>
2015-04-10 00:23:01 +0300

    ru-Ru

---
+++ CloudW/__init__.py
@@ -0,0 +1,10 @@
+from django.utils.translation import ugettext_lazy as _
+
+_(u'auth')
+_(u'configuration')
+_(u'sites')
+_(u'privatebroadcast')
+_(u'Auth')
+_(u'Configuration')
+_(u'Sites')
+_(u'supplement_registration')
