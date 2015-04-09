bb111f03f4fc84cf6dce18ac31626a6bdee406d2
stamigos <oldtigersvoice@gmail.com>
2015-04-09 14:17:14 +0300

    error_messages

--- supplement_registration/models.py
+++ supplement_registration/models.py
@@ -7,8 +7,9 @@ from django.contrib.auth.models import User
 
 class MyRegistrationSupplement(RegistrationSupplementBase):
 
-    first_name = models.CharField(u'Имя', max_length=100, help_text=u'Введите свое имя', error_messages={u'Ошибка':
-                                                                                                             u"Поле пустое"})
+    first_name = models.CharField(u'Имя', max_length=100, help_text=u'Введите свое имя', error_messages={
+        "required": u"Поле пустое"
+    })
     last_name = models.CharField(u'Фамилия', max_length=30, help_text=u'Введите свою фамилию')
     father_name = models.CharField(u'Отчество', max_length=30, help_text=u'Введите свое отчество')
     city = models.CharField(u'Город', max_length=30, help_text=u'Введите город')
