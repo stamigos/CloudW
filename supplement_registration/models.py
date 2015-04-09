352be7507112de099bd22c8739be72a1149cf814
stamigos <oldtigersvoice@gmail.com>
2015-04-09 14:05:06 +0300

    help_text

--- supplement_registration/models.py
+++ supplement_registration/models.py
@@ -7,7 +7,8 @@ from django.contrib.auth.models import User
 
 class MyRegistrationSupplement(RegistrationSupplementBase):
 
-    first_name = models.CharField(u'Имя', max_length=100, help_text=u'Введите свое имя')
+    first_name = models.CharField(u'Имя', max_length=100, help_text=u'Введите свое имя', error_messages={u'Ошибка':
+                                                                                                             u"Поле пустое"})
     last_name = models.CharField(u'Фамилия', max_length=30, help_text=u'Введите свою фамилию')
     father_name = models.CharField(u'Отчество', max_length=30, help_text=u'Введите свое отчество')
     city = models.CharField(u'Город', max_length=30, help_text=u'Введите город')
