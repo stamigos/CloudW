67859226e6ebc302391ae03f8e74542d061347d5
stamigos <oldtigersvoice@gmail.com>
2015-04-10 20:00:47 +0300

    many to many

--- privatebroadcast/models.py
+++ privatebroadcast/models.py
@@ -7,6 +7,7 @@ from ckeditor.fields import RichTextField
 #TODO: add DateField() for tasks
 class Task(models.Model):
     video = models.OneToOneField(MultiuploaderFile, blank=True)
+    video_multiple = models.ManyToManyField(MultiuploaderFile, blank=True)
     filename = models.CharField(max_length=30, blank=True)
     title = models.CharField(max_length=200)
     description = RichTextField()
