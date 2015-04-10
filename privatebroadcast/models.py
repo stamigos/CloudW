0586f92b3d2a3644d34f2c45cfa17010ef4f54d5
stamigos <oldtigersvoice@gmail.com>
2015-04-10 05:25:53 +0300

    design fixes

--- privatebroadcast/models.py
+++ privatebroadcast/models.py
@@ -6,7 +6,7 @@ from multiuploader.models import MultiuploaderFile
 #TODO: add DateField() for tasks
 class Task(models.Model):
     video = models.OneToOneField(MultiuploaderFile, blank=True)
-    filename = models.CharField(max_length=40, blank=True)
+    filename = models.CharField(max_length=30, blank=True)
     title = models.CharField(max_length=200)
     description = models.TextField()
     image = models.ImageField(upload_to='media/')
