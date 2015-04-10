9c161fa21d2fff0003e401b3476991525dd8b8fe
stamigos <oldtigersvoice@gmail.com>
2015-04-10 15:32:10 +0300

    neeed

--- supplement_registration/models.py
+++ supplement_registration/models.py
@@ -12,8 +12,8 @@ class MyRegistrationSupplement(RegistrationSupplementBase):
     father_name = models.CharField(u'Отчество', max_length=30) #help_text=u'Введите свое отчество')
     city = models.CharField(u'Город', max_length=30) #help_text=u'Введите город')
     email = models.EmailField(u'Email', max_length=100) #help_text=u'Введите email')
-   # phone_number = PhoneNumberField(u'Телефон', max_length=30) #help_text=u'Введите свой телефон')
-    phone_number = models.IntegerField(u'Телефон_', max_length=20)
+    phone_number = PhoneNumberField(u'Телефон', max_length=30) #help_text=u'Введите свой телефон')
+    #phone_number = models.IntegerField(u'Телефон_', max_length=20)
 
     def __unicode__(self):
         # a summary of this supplement
